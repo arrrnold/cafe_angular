6390c4b4db521cc4e912ed9b6b720c40d010ec17
arrrnold <tm02arnold@gmail.com>
2024-05-06 17:12:42 -0600

    Agregar métodos para registro y recuperación de contraseña en AuthService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,11 +6,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
   constructor(private http: HttpClient) { }
-  
+
   apiUrl = 'https://api-tt2-ktkr.onrender.com/v1/registro/' // url del modulo de auth
-  
+
   login(value: any) {
     return this.http.post(this.apiUrl + 'login', value)
+  }
+
+  register(value: any) {
+    return this.http.post(this.apiUrl + 'register', value)
+  }
+
+  enviarEmail(value: any) {
+    return this.http.post(this.apiUrl + 'enviarEmail', value)
+  }
+
+  cambiarClave(value: any) {
+    return this.http.post(this.apiUrl + 'cambiarClave', value)
   }
 
 }
