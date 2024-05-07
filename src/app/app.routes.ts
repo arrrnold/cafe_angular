19814a0c1a1e0ac079898acb7665dba38108ced4
arrrnold <tm02arnold@gmail.com>
2024-05-06 18:22:18 -0600

    Add dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     /*
@@ -11,5 +12,7 @@ export const routes: Routes = [
     { path: '', redirectTo: '/auth/login', pathMatch: 'full' }, // Redirigir a /auth/login por defecto
     { path: 'auth/login', component: LoginComponent },
     { path: 'auth/reset_password', component: ResetPasswordComponent },
+    // dashboard
+    { path: 'dashboard', component: DashboardComponent },
     { path: '**', redirectTo: '/auth/login' } // Redirigir a /auth/login si la ruta no existe
 ];
