68a7808c836c3053eb5eb5e1fea200549223601b
arrrnold <tm02arnold@gmail.com>
2024-05-09 16:23:24 -0600

    Actualizar enlaces de restablecimiento de contraseña y estilos en componente de inicio de sesión

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.css'
 })
 export class ResetPasswordComponent {
+
+  sendError = '';
+
+  onSendMail(formValue: any) {
+    // TODO: Implementar la lógica para enviar el correo electrónico
+    console.log('Correo electrónico enviado a: ' + formValue.email);
+  }
+
 
 }
