dffcaf39724a6e2b80c5e5f6aabc1a9db0462060
arrrnold <tm02arnold@gmail.com>
2024-05-30 17:58:13 -0600

    chore: Organizar código y corregir errores en el frontend de agregar producto

--- src/app/agregar-producto/agregar-producto.component.ts
+++ src/app/agregar-producto/agregar-producto.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {CommonModule, NgClass, NgForOf} from "@angular/common";
-import {NgxPaginationModule} from 'ngx-pagination';
-import {ProductosService} from "./productos.service";
-import {FormsModule} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgClass, NgForOf } from "@angular/common";
+import { NgxPaginationModule } from 'ngx-pagination';
+import { ProductosService } from "./productos.service";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-agregar-producto',
@@ -24,15 +24,17 @@ export class AgregarProductoComponent implements OnInit {
 
   respuesta: any;
   productos: any;
-  nombre = '';
-  precio = '';
-  
 
 
   ngOnInit() {
     this.productosService.getProductos().subscribe((data: any) => {
       this.respuesta = data;
       this.productos = this.respuesta.productos;
+
+      // si el estado del producto "visible" = true entonces se muestra en la tabla
+      this.productos.forEach((producto: any) => {
+        producto.visible = false;
+      });
 
       if (this.productos.recomendacion == true) {
         this.productos.recomendacion = "Si";
@@ -45,12 +47,23 @@ export class AgregarProductoComponent implements OnInit {
   page: number = 1;
   protected readonly Math = Math;
 
-  onAgregarProducto(value: any) {
-    // console.log(value);
-    
+  onAgregarProducto(producto: any) {
+    producto.visible = true;
+
+    // convertir la imagen del producto para guardarla en la bd y mostrarla en la tabla
+    const reader = new FileReader();
+    reader.readAsDataURL(producto.imagen);
+    reader.onload = () => {
+      producto.imagen = reader.result;
+    };
+
+
+    console.log(producto);
+
+
   }
 
-  onEditarProducto(value: any) {
-    console.log(value);
+  onEditarProducto(producto: any) {
+    console.log(producto);
   }
 }
