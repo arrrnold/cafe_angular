5bee449b767ff55455318d535078befd9c28f927
arrrnold <tm02arnold@gmail.com>
2024-06-11 00:03:00 -0600

    Actualizar componente de inicio de sesión para redirigir a la página de inicio según el rol del usuario

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,41 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { AuthService } from '../auth.service';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {AuthService} from '../auth.service';
 
 @Component({
-    selector: 'app-login',
-    standalone: true,
-    imports: [CommonModule, FormsModule],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css'
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
-    loginError: string = '';
+  loginError: string = '';
 
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+  }
 
-    ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
-    onLogin(formValue: any) {
-        this.authService.login(formValue)
-            .subscribe({
-                next: (res: any) => {
-                    if (res.estado == 1) {
-                        console.log('login exitoso');
-                        // Almacenar el token en el almacenamiento local
-                        this.authService.storeToken(res.token);
-                        // redirigir a la pagina de inicio
-                        window.location.href = '/dashboard';
-                    } else {
-                        console.log('login fallido');
-                        this.loginError = res.mensaje;
-                    }
-                },
-                error: (err: any) => {
-                    console.log('error en login');
-                    this.loginError = 'El inicio de sesión ha fallado. Por favor, inténtalo de nuevo.';
-                }
-            });
-    }
+  onLogin(formValue: any) {
+    this.authService.login(formValue)
+      .subscribe({
+        next: (res: any) => {
+          if (res.estado == 1) {
+            // si el usuario es administrador
+            if (res.rol == 'administrador') {
+              console.log('login exitoso');
+              // Almacenar el token en el almacenamiento local
+              this.authService.storeToken(res.token);
+              // redirigir a la pagina de inicio
+              window.location.href = '/dashboard';
+            } else {
+              console.log('login fallido');
+              this.loginError = 'No tienes permiso para acceder a esta página';
+            }
+          } else {
+            console.log('login fallido');
+            this.loginError = res.mensaje;
+          }
+        },
+        error: (err: any) => {
+          console.log('error en login');
+          this.loginError = 'El inicio de sesión ha fallado. Por favor, inténtalo de nuevo.';
+        }
+      });
+  }
 }
