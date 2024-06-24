2410ba858d744b8028412cf1b212f46429b5a9b5
arrrnold <tm02arnold@gmail.com>
2024-06-24 02:49:24 -0600

    Actualizar dependencias de npm y agregar scripts para imprimir y generar comprobantes

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -3,6 +3,8 @@ import { CommonModule, NgClass, NgForOf } from "@angular/common";
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ProductosService } from '../agregar-producto/productos.service';
+import jsPDF from 'jspdf';
+import 'svg2pdf.js';
 
 @Component({
   selector: 'app-pedidos',
@@ -13,19 +15,45 @@ import { ProductosService } from '../agregar-producto/productos.service';
     NgClass,
     FormsModule,
     CommonModule,
-    ReactiveFormsModule
-
-
+    ReactiveFormsModule,
   ],
   templateUrl: './pedidos.component.html',
   styleUrls: ['./pedidos.component.css']
 })
 export class PedidosComponent implements OnInit {
-
   estadoPedidoSeleccionado: string = "";
   idPedidoSeleccionado: number = 0;
 
+  constructor(private productosService: ProductosService) { }
 
+  mensajeExito: string = "";
+  mensajeError: string = "";
+  respuesta: any;
+
+  // variables para la paginacion
+  protected readonly Math = Math;
+  page: number = 1;
+  itemsPerPage: number = 5;
+  totalItems: number = 0;
+
+  pedidos: any = [];
+  pedidoCompleto: any = {};
+
+  ngOnInit(): void {
+    this.productosService.getPedidos().subscribe((respuesta: any) => {
+      this.pedidos = respuesta.pedidos.filter((p: any) => p.usuario !== null);
+
+      this.pedidos = this.pedidos.map((p: any) => {
+        if (p.usuario.Usuario.imagen_perfil === null) {
+          p.usuario.Usuario.imagen_perfil = "../../assets/img/foto_default.png";
+        }
+        return p;
+      });
+
+      // Actualizar el total de ítems para la paginación
+      this.totalItems = this.pedidos.length;
+    });
+  }
 
   cerrarSesion() {
     localStorage.removeItem('token');
@@ -36,14 +64,96 @@ export class PedidosComponent implements OnInit {
   cargarPedido(pedido: any) {
     this.estadoPedidoSeleccionado = pedido.pedido.estado;
     this.idPedidoSeleccionado = pedido.pedido.id;
-
+    this.pedidoCompleto = pedido;
   }
 
   imprimirComprobante() {
-    // segun los detalles del pedido cargado, se debe imprimir el comprobante en pdf usando la libreria jsPDF
+    const doc = new jsPDF();
 
-    // el pedido completo esta almecenado en 
+    const logoPath = '../../assets/img/logo.svg';
+    const loadSVG = (path: string) => {
+      return fetch(path)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.text();
+        });
+    };
 
+    const renderPDF = async () => {
+      try {
+        // Cargar el logo SVG y añadirlo al documento
+        const svg = await loadSVG(logoPath);
+        const svgElement = new DOMParser().parseFromString(svg, 'image/svg+xml').documentElement;
+        await doc.svg(svgElement, { x: 70, y: 10, width: 70, height: 35 });
+      } catch (error) {
+        console.error('Error loading SVG:', error);
+      }
+
+      let y = 60; // Ajustar para dar espacio al logo
+
+      // Función para texto con estilo
+      const textWithStyles = (text: string, isBold: boolean, x: number, y: number) => {
+        if (isBold) {
+          doc.setFont('Times New Roman', 'bold');
+        } else {
+          doc.setFont('Times New Roman', 'normal');
+        }
+        doc.text(text, x, y);
+      };
+
+      textWithStyles('Nombre:', true, 10, y);
+      textWithStyles(`${this.pedidoCompleto.usuario.Usuario.nombre_completo}`, false, 50, y);
+      y += 10;
+
+      textWithStyles('Correo:', true, 10, y);
+      textWithStyles(`${this.pedidoCompleto.usuario.Usuario.email}`, false, 50, y);
+      y += 10;
+
+      textWithStyles('Productos:', true, 10, y);
+      y += 10;
+      this.pedidoCompleto.productos.forEach((producto: any) => {
+        textWithStyles(`${producto.cantidad} x ${producto.nombre}`, false, 10, y);
+        y += 10;
+      });
+
+      textWithStyles('Preferencias:', true, 10, y);
+      textWithStyles(`${this.pedidoCompleto.pedido.preferencias}`, false, 50, y);
+      y += 10;
+
+      textWithStyles('Ubicación:', true, 10, y);
+      textWithStyles(`${this.pedidoCompleto.pedido.ubicacion}`, false, 50, y);
+      y += 10;
+
+      textWithStyles('Pagado:', true, 10, y);
+      textWithStyles(`${this.pedidoCompleto.pedido.pagado ? 'Sí' : 'No'}`, false, 50, y);
+      y += 10;
+
+      textWithStyles('Estado:', true, 10, y);
+      textWithStyles(`${this.pedidoCompleto.pedido.estado}`, false, 50, y);
+      y += 10;
+
+      textWithStyles('Fecha:', true, 10, y);
+      textWithStyles(`${new Date(this.pedidoCompleto.pedido.fecha).toLocaleDateString('es-ES')}`, false, 50, y);
+      y += 10;
+
+      textWithStyles('Cantidad a pagar:', true, 10, y);
+      textWithStyles(`$${this.pedidoCompleto.pedido.cant_a_pagar}`, false, 70, y);
+      y += 10;
+
+      textWithStyles('Propina:', true, 10, y);
+      textWithStyles(`$${this.pedidoCompleto.pedido.propina}`, false, 70, y);
+      y += 10;
+
+      textWithStyles('Total con propina:', true, 10, y);
+      textWithStyles(`$${this.pedidoCompleto.pedido.total}`, false, 70, y);
+      y += 10;
+
+      doc.save('comprobante.pdf');
+    };
+
+    renderPDF();
   }
 
   cambiarEstado() {
@@ -63,39 +173,6 @@ export class PedidosComponent implements OnInit {
       } else {
         this.mensajeError = "Error al actualizar el estado";
       }
-    });
-  }
-
-  constructor(private productosService: ProductosService) { }
-
-  mensajeExito: string = "";
-  mensajeError: string = "";
-  respuesta: any;
-
-  // variables para la paginacion
-  protected readonly Math = Math;
-  page: number = 1;
-  itemsPerPage: number = 5;
-  totalItems: number = 0;
-
-  pedidos: any = [];
-  pedidoCompleto: any = {};
-  cosas: any = {};
-
-  ngOnInit(): void {
-    this.productosService.getPedidos().subscribe((respuesta: any) => {
-      this.pedidos = respuesta.pedidos.filter((p: any) => p.usuario !== null);
-
-      this.pedidos = this.pedidos.map((p: any) => {
-        if (p.usuario.Usuario.imagen_perfil === null) {
-          p.usuario.Usuario.imagen_perfil = "../../assets/img/foto_default.png";
-        }
-        return p;
-      });
-
-      // Actualizar el total de ítems para la paginación
-      this.totalItems = this.pedidos.length;
-
     });
   }
 }
